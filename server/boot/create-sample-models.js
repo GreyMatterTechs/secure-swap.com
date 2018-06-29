e1edea32f3dbb906ace5d4673c51003419904e3d
philippe-gmt <philippe@aubessard.net>
2018-06-29 22:29:13 +0200

    gestion Date ICO

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -5,6 +5,9 @@ var async		= require('async');
 var db;
 var dbName;
 var mAdmin;
+var mPurchase;
+var mRole;
+var mRoleMapping;
 
 var HOUR_IN_MILLISECONDS = 1000 * 60 * 60;
 var DAY_IN_MILLISECONDS = HOUR_IN_MILLISECONDS * 24;
@@ -14,17 +17,81 @@ var YEAR_IN_MILLISECONDS = DAY_IN_MILLISECONDS * 365;
 var hour = 1;
 
 
+
+function updateRole(cb) {
+	mRole.find({where: {name: 'admin'}}, function(err, roles) {
+		if (err) return cb(err);
+		if (roles && roles.length === 1) return cb(null, roles[0]);
+		mRole.create({
+			name: 'admin'
+		 }, function(err, role) {
+			if (err) return cb(err);
+			return cb(null, role);
+		});
+	});
+}
+
+function updatePurchases(cb) {
+
+	mPurchase.findById(1, function(err, inst) {
+		if (err) return cb(err);
+		if (inst) return cb(null, inst);
+		mPurchase.create({
+			softCap:  5000000,
+			hardCap: 30000000,
+			tokensTotal: 1000000,
+			tokensSold: 314358,
+			start: new Date(2019,1,0).getTime()
+		}, function(err, purchase) {
+			if (err) return cb(err);
+			return cb(null, purchase);
+		});
+	});
+
+}
+
 function updateAdmins(cb) {
+
+	function createRole(user, cb) {
+		mRole.find({where: {name: 'admin'}}, function(err, roles) {
+			if (err) return cb(err);
+			if (roles && roles.length === 1) {
+				var mPrincipals = roles[0].principals;
+				mPrincipals.find({where: {principalId: user.id}}, function(err, principals) {
+					if (err) return cb(err);
+					if (principals && principals.length === 1) return cb(null, principals[0]);
+					mPrincipals.create({
+						principalType: mRoleMapping.USER,
+						principalId: user.id
+					}, function(err, principal) {
+						if (err) return cb(err);
+						return cb(null, principal);
+					});
+				});
+			} else {
+				return cb('Can\'t find Role admin');
+			}
+		});
+	}
 
 	function findOrCreate(account) {
 		return new Promise(function(account, resolve, reject) {
 			mAdmin.findOne({where: {email: account.userdata.email}}, function(err, inst) {
 				if (err) return reject(err);
-				if (inst) return resolve(inst);
-				mAdmin.create(account.userdata, function(err, user) {
-					if (err) return reject(err);
-					return resolve(user);
-				});
+				if (inst) {
+					createRole(inst, function(err, principal) {
+						if (err) return reject(err);
+						return resolve(inst);
+					});	
+				} else {
+					mAdmin.create(account.userdata, function(err, user) {
+						if (err) return reject(err);
+						createRole(user, function(err, principal) {
+							if (err) return reject(err);
+							return resolve(user);
+						});	
+					});
+				}
 			});
 		}.bind(null, account));
 	}
@@ -86,12 +153,13 @@ function update(db, lbUpdateTables) {
 		if (err) throw err;
 		console.log('Loopback tables [' + lbUpdateTables + '] created in "' + dbName + '" database');
 		//create all models
-		async.parallel({
-			admins: async.apply(updateAdmins)
-		}, function (err, results) {
-			if (err) throw err;
-			console.log('> models Admin created sucessfully');
-		});
+
+		async.series([updateRole, updatePurchases, updateAdmins],
+			function(err, result) {
+				if (err) throw err;
+				console.log('> tables updated sucessfully');
+			}
+		);
 	});	
 }
 
@@ -100,15 +168,18 @@ module.exports = function (app) {
 	db = app.dataSources.db;
 	dbName = db.settings.host ? db.settings.host : db.settings.file;
 	mAdmin = app.models.Admin;
-	
+	mPurchase = app.models.Purchase;
+	mRole = app.models.Role;
+	mRoleMapping = app.models.RoleMapping;
+
 	var lbMigrateTables;
 	var lbUpdateTables;
 
 	if (process.env.NODE_ENV==='production') {
 		// on touche à rien
 	} else {
-		lbMigrateTables = ['User', 'ACL', 'RoleMapping', 'Role'];
-		lbUpdateTables = ['AccessToken', 'Admin'];
+		lbMigrateTables = ['User', 'ACL', 'RoleMapping'];
+		lbUpdateTables = ['AccessToken', 'Role', 'Admin', 'Purchase'];
 		create(db, lbMigrateTables);
 		update(db, lbUpdateTables);
 	}
