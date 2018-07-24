04b149710f4da1a22b427da0b1cd6af3c095eab5
unknown <philippe@aubessard.net>
2018-07-24 17:11:25 +0200

    highcharts test

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -250,6 +250,69 @@
 				});
 
 				//--------------------------------------------------------------------------------------------------------------
+				// Token distribution
+				//--------------------------------------------------------------------------------------------------------------
+
+				// Radialize the colors
+				Highcharts.setOptions({
+					colors: Highcharts.map(Highcharts.getOptions().colors, function(color) {
+						return {
+							radialGradient: {
+								cx: 0.5,
+								cy: 0.3,
+								r: 0.7
+							},
+							stops: [
+								[0, color],
+								[1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+							]
+						};
+					})
+				});
+
+				// Build the chart
+				Highcharts.chart('token-dist-chart', {
+					chart: {
+						backgroundColor: 'transparent',
+						plotBackgroundColor: null,
+						plotBorderWidth: null,
+						plotShadow: true,
+						type: 'pie'
+					},
+				//	title: {
+				//		text: 'Browser market shares in January, 2018'
+				//	},
+				//	tooltip: {
+				//		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+				//	},
+					plotOptions: {
+						pie: {
+				//			allowPointSelect: true,
+				//			cursor: 'pointer',
+							dataLabels: {
+								enabled: true,
+								format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+								style: {
+									color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+								},
+								connectorColor: 'silver'
+							}
+						}
+					},
+					series: [{
+						name: 'Distribution',
+						data: [
+							{ name: 'Crowdsale', y: 61.41 },
+							{ name: 'Team', y: 11.84 },
+							{ name: 'Advisors', y: 10.85 },
+							{ name: 'Program', y: 4.67 },
+							{ name: 'BugBounty', y: 4.18 },
+							{ name: 'Other', y: 7.05 }
+						]
+					}]
+				});
+
+				//--------------------------------------------------------------------------------------------------------------
 				// Contact Form
 				//--------------------------------------------------------------------------------------------------------------
 
