56f89825412e614120ea7fc8e6b2307a70ef6451
philippe-gmt <philippe@aubessard.net>
2018-08-08 00:56:41 +0200

    texts fixes

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -37,7 +37,7 @@ function updateICO(cb) {
 	var icoData = {
 		state: 1,
 		wallet: '0xC50E31926CAf2cd7C69547cB2C1Bb127cB782E30',
-		tokenName: 'SSWT',
+		tokenName: 'SSW',
 		tokenPriceUSD: 0.45,
 		tokenPriceETH: 0.15414,
 		softCap: 10000000,
